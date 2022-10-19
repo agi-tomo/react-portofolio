82882048c245bf4a3fd8877635bab788ef987c5d
Margi Nur Utomo <agiplaza123@gmail.com>
2022-10-19 20:49:12 +0800

    add components

--- pages/index.js
+++ pages/index.js
@@ -1,23 +1,13 @@
 import Head from "next/head";
 import Image from "next/image";
 import styles from "../styles/Home.module.css";
+import Header from "../components/Header";
 
 export default function Home() {
     return (
         <>
-            <header>
-                <ul>
-                    <li>
-                        <a href="/">Home</a>
-                    </li>
-                    <li>
-                        <a href="/blog">Blog</a>
-                    </li>
-                    <li>
-                        <a href="/users">Users</a>
-                    </li>
-                </ul>
-            </header>
+            <Header />
+            <h1 className={styles["title"]}>Halo ini halaman Home</h1>
         </>
     );
 }
