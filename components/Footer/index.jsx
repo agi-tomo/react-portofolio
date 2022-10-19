555dc567638e32c8a84b35516f93a4cb5d853477
Margi Nur Utomo <agiplaza123@gmail.com>
2022-10-20 07:46:03 +0800

    add header / footer

---
+++ components/Footer/index.jsx
@@ -0,0 +1,9 @@
+import styles from "./Footer.module.css";
+
+export default function Footer() {
+    return (
+        <>
+            <h1 className={styles.title}>Margi Nur Utomo 2022</h1>
+        </>
+    );
+}
--- pages/index.js
+++ pages/index.js
@@ -2,12 +2,14 @@ import Head from "next/head";
 import Image from "next/image";
 import styles from "../styles/Home.module.css";
 import Header from "../components/Header";
+import Footer from "../components/Footer";
 
 export default function Home() {
     return (
         <>
             <Header />
             <h1 className={styles["title"]}>Halo ini halaman Home</h1>
+            <Footer />
         </>
     );
 }
