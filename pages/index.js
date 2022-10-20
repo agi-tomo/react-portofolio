3885d264c07f7d05d58b9b2079a9083c07af127e
Margi Nur Utomo <agiplaza123@gmail.com>
2022-10-20 10:59:33 +0800

    add feature

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,26 @@
 import Header from "../components/Header";
 import Footer from "../components/Footer";
+import styles from "../styles/Home.module.css";
+import Link from "next/link";
 
 export default function Home() {
     return (
         <>
             <Header />
+            <main className={styles.main}>
+                <h1 className={styles.title}>Welcome to My Blog</h1>
+                <div className={styles.grid}>
+                    <div className={styles.card}>
+                        <Link href="/posts/first-post">
+                            <h3>First Post</h3>
+                        </Link>
+                        <p>
+                            Lorem Ipsum is simply dummy text of the printing and
+                            typesetting industry...
+                        </p>
+                    </div>
+                </div>
+            </main>
             <Footer />
         </>
     );
